9ad84825389f837de3eda4456bf43c6daab23de8
J Cho <jihyunc@live.com>
2019-10-07 23:38:15 -0400

    initial commit for int-checkbox

---
+++ src/components/form-controls/int-checkbox/int-checkbox.spec.ts
@@ -0,0 +1,7 @@
+import { IntCheckbox } from './int-checkbox';
+
+describe('int-checkbox', () => {
+  it('builds', () => {
+    expect(new IntCheckbox()).toBeTruthy();
+  });
+});
---
+++ src/components/form-controls/int-checkbox/int-checkbox.tsx
@@ -0,0 +1,57 @@
+import { Component, Host, h } from '@stencil/core';
+
+@Component({
+  tag: 'int-checkbox',
+  styleUrl: 'int-checkbox.css',
+  shadow: true
+})
+export class IntCheckbox {
+  /**
+   * Own Properties
+   */
+
+  /**
+   * Reference to host HTML element.
+   */
+
+  /**
+   * State() variables
+   */
+
+  /**
+   * Public Property API
+   */
+
+  /**
+   * Events section
+   */
+
+  /**
+   * Component lifecycle events
+   */
+
+  /**
+   * Listeners
+   */
+
+   
+  /**
+   * Public methods API
+   */
+
+  /**
+   * Local methods
+   */
+
+  /**
+   * render() function
+   */
+  render() {
+    return (
+      <Host>
+        <slot></slot>
+      </Host>
+    );
+  }
+
+}
