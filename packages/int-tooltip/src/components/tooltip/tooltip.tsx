716fb48b23f82485b61945010ac1b9677475634e
Jacob Farber <jacob.farber@fleetcomplete.com>
2020-03-16 22:31:26 -0400

    Commit before upgrade

--- packages/int-tooltip/src/components/tooltip/tooltip.tsx
+++ packages/int-tooltip/src/components/tooltip/tooltip.tsx
@@ -1,4 +1,5 @@
 import { Component, Host, h, Prop } from '@stencil/core';
+import "@integral-ui/int-overlay";
 
 @Component({
   tag: 'int-tooltip',
@@ -12,14 +13,18 @@ export class Tooltip {
   
   @Prop()
   content: string;
-
+  
   @Prop()
   requireClose: boolean = false;
+
+  componentDidLoad() {
+    
+  }
 
   render() {
     return (
       <Host>
-        
+        <int-overlay>overlay</int-overlay>
         <slot></slot>
       </Host>
     );
